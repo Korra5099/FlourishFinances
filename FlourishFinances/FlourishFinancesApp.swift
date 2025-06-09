5ac9d0528e9fe584601aca238b299b18a726486f
korra <korraemail@gmail.com>
2025-06-09 15:14:22 -0500

    Initial Commit

---
+++ FlourishFinances/FlourishFinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlourishFinancesApp.swift
+//  FlourishFinances
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FlourishFinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
