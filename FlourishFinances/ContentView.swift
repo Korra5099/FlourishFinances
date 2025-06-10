c12901d77cc71e0432d59c7ea734128a78d4999e
Khira <khirafreeman2029@gmail.com>
2025-06-10 10:01:46 -0500

    new updates (og)

--- FlourishFinances/ContentView.swift
+++ FlourishFinances/ContentView.swift
@@ -1,1 +1,28 @@
+//
+//  ContentView.swift
+//  FlourishFinances
+//
+//  Created by Scholar on 6/9/25.
+//
 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("change 1")
+            Text("change 2")
+            Text("change 3")
+            Text("change 4/khira")
+            Text("change 4-Rona")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
