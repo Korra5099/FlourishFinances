b76125a33ca8bd71070b3f32f850f82db070d3b8
Rona <ronaopolo40@gmail.com>
2025-06-11 10:28:14 -0500

    added color, navigation link, navigation stack, ZStack, VStack, and background

--- FlourishFinances/LoginPage.swift
+++ FlourishFinances/LoginPage.swift
@@ -10,28 +10,37 @@ import SwiftUI
 struct LoginPage: View{
     @State private var name = ""
     var body: some View {
-        Image("Image")
-        .resizable(resizingMode: .stretch)
-            .aspectRatio(contentMode: .fit)
-            .imageScale(.large)
-            .clipShape(Circle())
-            Spacer()
-        TextField ("Enter Name Here", text: $name)
-            .padding(.leading, 12.0)
-            .frame(height: nil)
-            .multilineTextAlignment(.center)
-            .font(.title)
-        Spacer().frame(height: 300)
-            NavigationLink( destination: ContentView()) {
-                Text("Thank You")
-                
+        NavigationStack {
+            ZStack {
+                Color(.green).edgesIgnoringSafeArea(.all)
+                VStack {
+                    Image("Image")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .imageScale(.large)
+                        .clipShape(Circle())
+                    Spacer()
+                    Text ("Welcome")
+                        .padding(.leading, 12.0)
+                        .frame(height: nil)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                    Spacer().frame(height: 250)
+                 NavigationLink(destination: ContentView()) {
+                    Text("Thank You")
+                        .font(.title)
+                };
+                    
+                    
+                    
+                }
             }
-        }
-        
             
+        }
     }
-
-
+    }
+    
 #Preview {
     LoginPage()
 }
+
